30a0b977a933a2754bf14f2084f3a67260f35e1e
desirerevon <desire.gardner@gmail.com>
2019-08-07 09:51:54 -0500

    Addding if statement for selected spinner itemm

--- app/src/main/java/com/dgardner/endgame/FetchPinterest.java
+++ app/src/main/java/com/dgardner/endgame/FetchPinterest.java
@@ -1,4 +1,55 @@
 package com.dgardner.endgame;
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class FetchPinterest {
+public class FetchPinterest extends AsyncTask<Void, Void, Void> {
+    String pinData ="";
+    String parse ="";
+    String theParser ="";
+
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+
+        {
+            try {
+                ///////////////////////////////////////////////////////////////CREATE REQUEST
+                URL url = new URL("https://www.reddit.com/r/Marvel/hot.json");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                //////////////////////////////////////////////////////////////READ ALL LINES
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+                while (line != null) {
+                    line = bufferedReader.readLine();
+                    pinData = pinData + line;
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+    }
+
+    @Override
+    protected void onPostExecute (Void aVoid){
+        super.onPostExecute(aVoid);
+
+        MainActivity.textView.setText(this.pinData);
+
+
+    }
 }
--- app/src/main/java/com/dgardner/endgame/MainActivity.java
+++ app/src/main/java/com/dgardner/endgame/MainActivity.java
@@ -1,7 +1,7 @@
 package com.dgardner.endgame;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
@@ -16,8 +17,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     String[] api = {"Marvel Subreddit", "Pinterest"};
 
     public Spinner spinner;
-    public EditText enterText;
-    public Button searchButton;
+    public static EditText enterText;
+    public static Button searchButton;
+    public static TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +27,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         enterText = (EditText) findViewById(R.id.EditTextID);
+        textView = (TextView)findViewById(R.id.textViewID);
         searchButton = (Button) findViewById(R.id.searchButtonID);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FetchReddit pull = new FetchReddit();
+                pull.execute();
+            }
+        });
+
+
         spinner = (Spinner) findViewById(R.id.spinnerID);
-
-
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Connector
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, api);
         /////////////////////////////////////////////////////////////////////////////////////////////Set To Spinner
@@ -38,7 +49,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(MainActivity.this, "You can search through : " + api[position], Toast.LENGTH_LONG).show();
+                //Toast.makeText(MainActivity.this, "You can search through : " + api[position], Toast.LENGTH_LONG).show();
+
+                if (!(spinner.getSelectedItem().toString().equals("Marvel Subreddit"))){
+                    startActivity(new Intent(MainActivity.this, FetchReddit.class));
+
+                } if (spinner.getSelectedItem().toString().equals("Pnterest")){
+                    startActivity((new Intent(MainActivity.this, FetchPinterest.class)));
+
+
+                }else if (spinner.getSelectedItem().toString().equals("")){
+
+                    Toast.makeText(MainActivity.this, "You have not made a selection: " +api[position], Toast.LENGTH_LONG).show();
+                }
+
 
             }
 
@@ -52,6 +76,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
 
     }
 }
